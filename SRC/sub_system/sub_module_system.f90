650e6ae201eb8f9fcd80690362913e4cc26a42a3
lauvergn <David.Lauvergnat@universite-paris-saclay.fr>
2024-05-09 08:30:38 +0200

    fix a bug on a variable name

--- SRC/sub_system/sub_module_system.f90
+++ SRC/sub_system/sub_module_system.f90
@@ -115,7 +115,7 @@ character (len=Line_len) :: compiler_libs = &
   integer :: BasisTOGrid_omp,BasisTOGrid_maxth,BasisTOGrid_maxth_init
   integer :: Grid_omp,Grid_maxth,Grid_maxth_init
   integer :: SG4_omp,SG4_maxth,SG4_maxth_init
-  integer :: CRP_omp,CRP_maxth,CRP_maxth_ini
+  integer :: CRP_omp,CRP_maxth,CRP_maxth_init
 
   logical :: Tune_SG4_omp  = .FALSE.
   logical :: Tune_Grid_omp = .FALSE.
